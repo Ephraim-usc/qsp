7c5ee90177c1822ad306792711ab084741ea3523
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-07-24 15:42:21 +0800

    Update gsp.py

--- qsp/gsp.py
+++ qsp/gsp.py
@@ -13,25 +13,30 @@ units.avagadro = unum.new_unit('avagadro', 6.0221415e23 / units.mol)
 
 
 class System:
-  def __init__(self, compartments, analytes, volumes = None):
-    self.compartments = compartments
-    self.n_compartments = len(compartments)
+  def __init__(self, analytes, compartments, volumes = None):
     self.analytes = analytes
     self.n_analytes = len(analytes)
-
-    if volumes is None:
-      self.volumes = np.full([self.n_compartments, self.n_analytes], np.nan)
-    else:
-      self.volumes = volumes
+    self.compartments = compartments
+    self.n_compartments = len(compartments)
     
-    self.flows = np.zeros([self.n_compartments, self.n_compartments, self.n_analytes])
+    self.volumes = np.full([self.n_analytes, self.n_compartments], np.nan)
+    self.flows = np.zeros([self.n_analytes, self.n_compartments, self.n_compartments])
     self.reactions = []
   
-  def set_volume(self, compartment, analyte, volume):
-    self.volumes[self.compartments.index(compartment), self.analytes.index(analyte)] = volume
+  def set_volumes(self, volumes):
+    volumes = np.array([[(i.number(units.l) if type(i) is unum.Unum else i) for i in row] for row in volumes])
+    self.volumes = volumes
   
-  def add_flow(self, compartment_a, compartment_b, analyte, coefficient):
-    self.flows[self.compartments.index(compartment_a), self.compartments.index(compartment_b), self.analytes.index(analyte)] += coefficient
+  def set_volume(self, compartment, analyte, volume):
+    volume = volume.number(units.l) if type(volume) is unum.Unum else volume
+    self.volumes[self.analytes.index(analyte), self.compartments.index(compartment)] = volume
+  
+  # set compartment_dest as None for clearance
+  def add_flow(self, compartment_source, compartment_dest, analyte, coefficient):
+    self.flows[self.analytes.index(analyte), self.compartments.index(compartment_source), self.compartments.index(compartment_source), ] -= coefficient
+    if compartment_dest is None:
+      return
+    self.flows[self.analytes.index(analyte), self.compartments.index(compartment_source), self.compartments.index(compartment_dest)] += coefficient
   
   # powers: dict of powers for each analyte
   # deltas: dict of concentration changes of the analytes per reaction
@@ -52,6 +57,10 @@ class System:
 
 compartments = ["central", "peripheral", "extracellular", "intracellular"]
 analytes = ["adc", "drug"]
+volumes = np.array([[0.084 * units.l, 0.136 * units.l], [0.051 * units.l, 0.523 * units.l], [np.nan, np.nan], [np.nan, np.nan]])
+
+volumes = np.array([[0.084 * units.l, 0.136 * units.l], [0.051 * units.l, 0.523 * units.l], [np.nan, np.nan], [np.nan, np.nan]])
 
 system = System(compartments, analytes)
+system.set_volumes(volumes)
 system.add_flow("central", "peripheral", "drug", 3.2)
