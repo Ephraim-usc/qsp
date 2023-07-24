958547290d6fc4d3c0b8d5e27595a257cd37d79f
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-07-24 15:15:02 +0800

    Update gsp.py

--- qsp/gsp.py
+++ qsp/gsp.py
@@ -13,21 +13,21 @@ units.avagadro = unum.new_unit('avagadro', 6.0221415e23 / units.mol)
 
 
 class System:
-  def __init__(self, compartments, analytes):
+  def __init__(self, compartments, analytes, volumes = None):
     self.compartments = compartments
     self.n_compartments = len(compartments)
     self.analytes = analytes
     self.n_analytes = len(analytes)
     
     self.volumes = np.full([self.n_compartments, self.n_analytes], np.nan)
-    self.flows = np.zeros([self.n_compartments, self.n_compartments], np.nan)
+    self.flows = np.zeros([self.n_compartments, self.n_compartments, self.n_analytes])
+    self.reactions = []
   
-  def set_volume(self, compartment, volumes):
-    self.compartments.append(compartment)
-    self.volumes.append(volumes)
+  def set_volume(self, compartment, analyte, volume):
+    self.volumes[self.compartments.index(compartment), self.analytes.index(analyte)] = volume
   
-  def add_flow(self, analyte, compartment_a, compartment_b, coefficient):
-    self.flows.append()
+  def add_flow(self, compartment_a, compartment_b, analyte, coefficient):
+    self.flows[self.compartments.index(compartment_a), self.compartments.index(compartment_b), self.analytes.index(analyte)] += coefficient
   
   # powers: dict of powers for each analyte
   # deltas: dict of concentration changes of the analytes per reaction
@@ -36,15 +36,18 @@ class System:
   
   def print(self):
     volumes = pd.DataFrame(self.volumes, index = self.compartments, columns = self.analytes)
+    print("<volumes>", flush = True)
     print(volumes, flush = True)
-
-
-def antigen_on(params):
-  return 5 * params["adc"] * params["drug"]
+    print(" ", flush = True)
+    for i, analyte in enumerate(self.analytes):
+      flows = pd.DataFrame(self.flows[:,:,i], index = self.compartments, columns = self.compartments)
+      print(f"<flow of {analyte}>", flush = True)
+      print(flows, flush = True)
+      print(" ", flush = True)
 
 
 compartments = ["central", "peripheral", "extracellular", "intracellular"]
 analytes = ["adc", "drug"]
 
 system = System(compartments, analytes)
-
+system.add_flow("central", "peripheral", "drug", 3.2)
