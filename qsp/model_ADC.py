33c0003d17b98e14087e6a250fb7715781cd7d8c
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-08-04 16:51:46 +0800

    Update model_ADC.py

--- qsp/model_ADC.py
+++ qsp/model_ADC.py
@@ -1,3 +1,5 @@
+from .qsp import *
+
 organs = ["heart", "lung", "muscle", "skin", "adipose", "bone", "brain", "kidney", "liver", "SI", "LI", "pancreas", "thymus", "spleen", "other"]
 tissues = ["plasma", "BC", "interstitial", "endosomal", "membrane", "cellular"]
 centrals = ["plasma", "BC", "lymph"]
@@ -6,7 +8,7 @@ compartments = centrals + [f"{organ}_{tissue}" for organ in organs for tissue in
 def model(host, target, linker, drug, DAR):
   analytes = ["adc", "drug"]
   system = System(analytes, compartments)
-
+  
   for analyte in analytes:
     for compartment in compartments:
       system.set_volume(analyte, compartment, host[f"volume_{compartment}"])
@@ -45,7 +47,7 @@ def model(host, target, linker, drug, DAR):
     system.add_flow("adc", f"{organ}_plasma", f"{organ}_interstitial", host[f"lymphatic_flow_{organ}"] * (1 - host[f"vascular_reflection_{organ}"]))
     system.add_flow("adc", f"{organ}_interstitial", "lymph", host[f"lymphatic_flow_{organ}"] * (1 - host["lymphatic_reflection"]))
     system.add_flow("adc", "lymph", "plasma", host[f"lymphatic_flow_{organ}"]) # the article used a different measure, which I think is weird
-
+  
   # endosomal take-up and plasma recycle
   for organ in organs:
     v = system.get_volume("adc", f"{organ}_endosomal")
@@ -63,10 +65,10 @@ def model(host, target, linker, drug, DAR):
   
   # fast drug equilibrium between tissues
   for organ in organs:
-    system.add_flow("MMAE", f"{organ}_plasma", f"{organ}_endosomal", host[f"plasma_flow_{organ}"] * drug["unbound_plasma"] * 1000)
-    system.add_flow("MMAE", f"{organ}_interstitial", f"{organ}_endosomal", host[f"plasma_flow_{organ}"] * 1000)
-    system.add_flow("MMAE", f"{organ}_endosomal", f"{organ}_plasma", host[f"plasma_flow_{organ}"] * 1000)
-    system.add_flow("MMAE", f"{organ}_endosomal", f"{organ}_interstitial", host[f"plasma_flow_{organ}"] * 1000)
+    system.add_flow("drug", f"{organ}_plasma", f"{organ}_endosomal", host[f"plasma_flow_{organ}"] * drug["unbound_plasma"] * 1000)
+    system.add_flow("drug", f"{organ}_interstitial", f"{organ}_endosomal", host[f"plasma_flow_{organ}"] * 1000)
+    system.add_flow("drug", f"{organ}_endosomal", f"{organ}_plasma", host[f"plasma_flow_{organ}"] * 1000)
+    system.add_flow("drug", f"{organ}_endosomal", f"{organ}_interstitial", host[f"plasma_flow_{organ}"] * 1000)
   
   # BC and cellular permeation
   v = host["volume_BC"]
@@ -76,7 +78,7 @@ def model(host, target, linker, drug, DAR):
   for organ in organs:
     v = host[f"volume_{organ}_BC"]
     system.add_flow("drug", f"{organ}_plasma", f"{organ}_BC", drug["permeability_BC"] * v * drug["unbound_plasma"])
-    system.add_flow("drug", f"{organ}_BC", f"{organ}_plasma", drug["permeability_BC"] * v * drug["unbound_BC"]
+    system.add_flow("drug", f"{organ}_BC", f"{organ}_plasma", drug["permeability_BC"] * v * drug["unbound_BC"])
   
   for organ in organs:
     v = host[f"volume_{organ}_cellular"]
@@ -111,4 +113,4 @@ def model(host, target, linker, drug, DAR):
     system.add_reaction(f"{organ}_cellular", degradation_cellular)
   
   system.add_reaction("plasma", dissociation)
-  
+
