65230f001c151dd110ea3aac16678b5e6356ce60
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-08-28 16:27:22 +0800

    Update adc2.py

--- qsp/adc2.py
+++ qsp/adc2.py
@@ -3,45 +3,45 @@ from .qsp import *
 from scipy.optimize import fsolve
 
 organs = ["heart", "lung", "muscle", "skin", "adipose", "bone", "brain", "kidney", "liver", "SI", "LI", "pancreas", "thymus", "spleen", "other"]
-tissues = ["plasma", "BC", "interstitial", "endosomal", "membrane", "cellular"]
+tissues = ["plasma", "BC", "interstitial", "endosomal", "cellular"]
 centrals = ["plasma", "BC", "lymph"]
 compartments = centrals + [f"{organ}_{tissue}" for organ in organs for tissue in tissues]
 
 ################### hosts ###################
 volumes_mouse = np.array([0.944, 0.773, 0.113,
-                          0.00585, 0.00479, 0.0217, 0.000760, 0.0217, 0.119,
-                          0.0295, 0.0241, 0.0384, 0.00102, 0.0384, 0.111,
-                          0.249, 0.204, 1.47, 0.0566, 1.47, 9.34,
-                          0.188, 0.154, 1.66, 0.0251, 1.66, 3.00,
-                          0.0218, 0.0178, 0.337, 0.00991, 0.337, 1.60,
-                          0.0621, 0.0508, 0.525, 0.0141, 0.525, 2.17,
-                          0.0107, 0.00873, 0.0873, 0.00243, 0.0873, 0.376,
-                          0.0289, 0.0236, 0.0788, 0.00263, 0.0788, 0.391,
-                          0.164, 0.134, 0.385, 0.00963, 0.385, 1.23,
-                          0.0116, 0.00950, 0.127, 0.00364, 0.127, 0.577,
-                          0.0050, 0.00409, 0.0545, 0.00157, 0.0545, 0.248,
-                          0.00534, 0.00437, 0.0169, 0.000485, 0.0169, 0.0699,
-                          0.0005, 0.000405, 0.00153, 0.00005, 0.00153, 0.00653, 
-                          0.0154, 0.0126, 0.0254, 0.000635, 0.0254, 0.0730,
-                          0.0195, 0.0160, 0.0797, 0.00233, 0.0797, 0.348,
+                          0.00585, 0.00479, 0.0217, 0.000760, 0.119,
+                          0.0295, 0.0241, 0.0384, 0.00102, 0.111,
+                          0.249, 0.204, 1.47, 0.0566, 9.34,
+                          0.188, 0.154, 1.66, 0.0251, 3.00,
+                          0.0218, 0.0178, 0.337, 0.00991, 1.60,
+                          0.0621, 0.0508, 0.525, 0.0141, 2.17,
+                          0.0107, 0.00873, 0.0873, 0.00243, 0.376,
+                          0.0289, 0.0236, 0.0788, 0.00263, 0.391,
+                          0.164, 0.134, 0.385, 0.00963, 1.23,
+                          0.0116, 0.00950, 0.127, 0.00364, 0.577,
+                          0.0050, 0.00409, 0.0545, 0.00157, 0.248,
+                          0.00534, 0.00437, 0.0169, 0.000485, 0.0699,
+                          0.0005, 0.000405, 0.00153, 0.00005, 0.00653, 
+                          0.0154, 0.0126, 0.0254, 0.000635, 0.0730,
+                          0.0195, 0.0160, 0.0797, 0.00233, 0.348,
                          ]) * units.ml
 
 volumes_human = np.array([3126, 2558, 274,
-                          13.1, 10.8, 48.8, 1.71, 48.8, 267,
-                          55.0, 45.0, 300, 5.00, 300, 595,
-                          662, 541, 3910, 150, 3910, 24815,
-                          127, 104, 1125, 17.0, 1125, 2035,
-                          148, 121, 2289, 67.3, 2289, 10840,
-                          224, 183, 1891, 50.8, 1891, 7817,
-                          31.9, 26.1, 261, 7.25, 261, 1124,
-                          18.2, 14.9, 49.8, 1.66, 49.8, 247,
-                          183, 149, 429, 10.7, 429, 1371,
-                          6.15, 5.03, 67.1, 1.93, 67.1, 305,
-                          8.74, 7.15, 95.3, 2.74, 95.3, 434,
-                          5.70, 4.66, 18.0, 0.518, 18.0, 74.7,
-                          0.353, 0.288, 1.09, 0.0321, 1.09, 4.65,
-                          26.8, 21.9, 44.3, 1.11, 44.3, 127,
-                          204, 167, 831, 24.3, 831, 3626
+                          13.1, 10.8, 48.8, 1.71, 267,
+                          55.0, 45.0, 300, 5.00, 595,
+                          662, 541, 3910, 150, 24815,
+                          127, 104, 1125, 17.0, 2035,
+                          148, 121, 2289, 67.3, 10840,
+                          224, 183, 1891, 50.8, 7817,
+                          31.9, 26.1, 261, 7.25, 1124,
+                          18.2, 14.9, 49.8, 1.66, 247,
+                          183, 149, 429, 10.7, 1371,
+                          6.15, 5.03, 67.1, 1.93, 305,
+                          8.74, 7.15, 95.3, 2.74, 434,
+                          5.70, 4.66, 18.0, 0.518, 74.7,
+                          0.353, 0.288, 1.09, 0.0321, 4.65,
+                          26.8, 21.9, 44.3, 1.11, 127,
+                          204, 167, 831, 24.3, 3626
                          ]) * units.ml
 
 plasma_flows_mouse = np.array([36.5, 373, 86.1, 27.8, 13.4, 15.2, 11.8, 68.5, 10.3, 58.1, 17.3, 6.24, 1.19, 8.18, 10.9]) * units.ml/units.h
@@ -59,7 +59,10 @@ vascular_reflections = np.array([0.95, 0.95, 0.95, 0.95, 0.95, 0.85, 0.99, 0.9, 
 lymphatic_reflection = 0.2
 
 endosomal_pinocytosis = 3.66e-2 / units.h
-plasma_recycle = 0.715
+vascular_recycle = 0.715
+FcRn = 49.8 * units.micromolar
+FcRn_on = 0.0806 * 1/units.nM/units.d
+FcRn_off = 6.55 / units.h
 
 mouse = {}
 mouse.update({f"volume_{compartment}":x for compartment, x in zip(compartments, volumes_mouse)})
@@ -69,7 +72,8 @@ mouse.update({f"lymphatic_flow_{organ}":x for organ, x in zip(organs, lymphatic_
 mouse.update({f"cell_density_{organ}":x for organ, x in zip(organs, cell_densities)})
 mouse.update({f"vascular_reflection_{organ}":x for organ, x in zip(organs, vascular_reflections)})
 mouse.update({"lymphatic_reflection":lymphatic_reflection})
-mouse.update({"endosomal_pinocytosis":endosomal_pinocytosis, "plasma_recycle":plasma_recycle})
+mouse.update({"endosomal_pinocytosis":endosomal_pinocytosis, "vascular_recycle":vascular_recycle})
+mouse.update({"FcRn": FcRn, "FcRn_on": FcRn_on, "FcRn_off": FcRn_off})
 
 human = {}
 human.update({f"volume_{compartment}":x for compartment, x in zip(compartments, volumes_human)})
@@ -79,8 +83,8 @@ human.update({f"lymphatic_flow_{organ}":x for organ, x in zip(organs, lymphatic_
 human.update({f"cell_density_{organ}":x for organ, x in zip(organs, cell_densities)})
 human.update({f"vascular_reflection_{organ}":x for organ, x in zip(organs, vascular_reflections)})
 human.update({"lymphatic_reflection":lymphatic_reflection})
-human.update({"endosomal_pinocytosis":endosomal_pinocytosis, "plasma_recycle":plasma_recycle})
-
+human.update({"endosomal_pinocytosis":endosomal_pinocytosis, "vascular_recycle":vascular_recycle})
+human.update({"FcRn": FcRn, "FcRn_on": FcRn_on, "FcRn_off": FcRn_off})
 
 ################### targets ###################
 # organs = ["heart", "lung", "muscle", "skin", "adipose", "bone", "brain", "kidney", "liver", "SI", "LI", "pancreas", "thymus", "spleen", "other"]
@@ -202,8 +206,11 @@ def model(host, target, linker, drug):
     v = system.get_volume("adc", f"{organ}_endosomal")
     system.add_flow("adc", f"{organ}_plasma", f"{organ}_endosomal", host["endosomal_pinocytosis"] * v)
     system.add_flow("adc", f"{organ}_interstitial", f"{organ}_endosomal", host["endosomal_pinocytosis"] * v)
-    system.add_flow("adc", f"{organ}_endosomal", f"{organ}_plasma", host["endosomal_pinocytosis"] * v * host["plasma_recycle"])
-    system.add_flow("adc", f"{organ}_endosomal", f"{organ}_interstitial", host["endosomal_pinocytosis"] * v * (1 - host["plasma_recycle"]))
+    
+    system.set_x("FcRn", f"{organ}_endosomal", host["FcRn"])
+    system.add_reaction(f"{organ}_endosomal", {"adc":1, "FcRn":1}, {f"FcRn-adc":1}, host["FcRn_on"], backward = host["FcRn_off"])
+    system.add_reaction(f"{organ}_endosomal", {"FcRn-adc":1}, {"FcRn":1}, host["endosomal_pinocytosis"] * host["vascular_recycle"], side_compartment = f"{organ}_plasma", side_products = {"adc":1})
+    system.add_reaction(f"{organ}_endosomal", {"FcRn-adc":1}, {"FcRn":1}, host["endosomal_pinocytosis"] * (1 - host["vascular_recycle"]), side_compartment = f"{organ}_interstitial", side_products = {"adc":1})
   
   # target binding and internalization
   for organ in organs:
