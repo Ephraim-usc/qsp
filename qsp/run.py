27308e73c18bee1ed1c2e792ca47c94d19e89ebb
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-09-28 17:46:49 +0800

    Update run.py

--- qsp/run.py
+++ qsp/run.py
@@ -1,50 +1,50 @@
 from qsp import *
 from qsp.model_TCE import *
 
-pd.option_context(
-                  'display.max_rows', None,
-                  'display.max_columns', None,
-                  'display.precision', 6,
-                 )
+double = FTC238.copy()
+double["name"] = "double"
 
-for dose in [0.1, 1, 10, 100]:
-  for construct, construct_name in zip([R72, R77], ["R72", "R77"]):
-    system = model(human, construct, FTC238, [other, lung, SI])
-    system.add_x("mmm", "plasma", dose * units.nM)
-    system.run(300 * units.h, t_step = 1/60 * units.h, t_record = 1 * units.h)
+single = FTC238.copy()
+single["num_B"] = 0
+single["name"] = "single"
 
-    groups = [["C"],
-              ["A", "B"],
-              drugs,
-              [f"C-{drug}" for drug in drugs],
-              [f"{drug}-{target}" for drug in drugs for target in ["A", "B", "AB"]],
-              [f"C-{drug}-{target}" for drug in drugs for target in ["A", "B", "AB"]]]
-    labels = ["C", "target", "drug", "C-drug", "drug-target", "C-drug-target"]
-    colors = ["tab:orange", "tab:blue", "black", "wheat", "skyblue", "tab:purple"]
-    system.plot(compartments = ["plasma", "tumor_interstitial", "other_interstitial", "lung_interstitial", "SI_interstitial"],
-                groups = groups, labels = labels, colors = colors,
-                output = f"{dose}_{construct_name}_summary.png")
-    
-    '''
-    groups = [[f"{drug}"] +
-              [f"{CD3}-{drug}" for CD3 in ["CD3eff", "CD3reg"]] +
-              [f"{drug}-{target}" for target in ["A", "B", "AB"]] +
-              [f"{CD3}-{drug}-{target}" for CD3 in ["CD3eff", "CD3reg"] for target in ["A", "B", "AB"]]
-              for drug in drugs]
-    labels = [f"(CD3-){drug}(-target)" for drug in drugs]
-    colors = ["tab:green"] * 4
-    linestyles = ["solid", (0, (1, 1)), "dashed", "dotted"]
-    system.plot(compartments = ["plasma", "tumor_interstitial", "other_interstitial", "lung_interstitial", "SI_interstitial"],
-                groups = groups, labels = labels, colors = colors, linestyles = linestyles,
-                output = f"{dose}_{construct_name}_drugs.png")
-    '''
+def plot(system, name):
+  targets = ["A", "B", "AB"]
+  groups = [["C"],
+            ["A", "B"],
+            drugs,
+            [f"C-{drug}" for drug in drugs],
+            [f"{drug}-{target}" for drug in drugs for target in targets],
+            [f"C-{drug}-{target}" for drug in drugs for target in targets]]
+  labels = ["C", "target", "drug", "C-drug", "drug-target", "C-drug-target"]
+  colors = ["tab:orange", "tab:blue", "black", "wheat", "skyblue", "tab:purple"]
+  system.plot(compartments = ["plasma"] + [f"{tumor['name']}_interstitial" for tumor in system.tumors] + [f"{organ['name']}_interstitial" for organ in system.organs], 
+              groups = groups, labels = labels, colors = colors, 
+              output = f"{name}_summary.png")
+  
+  groups = [drugs + [f"{drug}-{target}" for drug in drugs for target in targets] + [f"C-{drug}" for drug in drugs] + [f"C-{drug}-{target}" for drug in drugs for target in targets],
+                [f"n{a}{b}" for a in ["m","n"] for b in ["m","n"]] + [f"n{a}{b}-{target}" for a in ["m","n"] for b in ["m","n"] for target in targets] + [f"C-n{a}{b}" for a in ["m","n"] for b in ["m","n"]] + [f"C-n{a}{b}-{target}" for a in ["m","n"] for b in ["m","n"] for target in targets],
+                [f"{c}n{b}" for c in ["m","n"] for b in ["m","n"]] + [f"{c}n{b}-{target}" for c in ["m","n"] for b in ["m","n"] for target in targets] + [f"C-{c}n{b}" for c in ["m","n"] for b in ["m","n"]] + [f"C-{c}n{b}-{target}" for c in ["m","n"] for b in ["m","n"] for target in targets],
+                [f"{c}{a}n" for c in ["m","n"] for a in ["m","n"]] + [f"{c}{a}n-{target}" for c in ["m","n"] for a in ["m","n"] for target in targets] + [f"C-{c}{a}n" for c in ["m","n"] for a in ["m","n"]] + [f"C-{c}{a}n-{target}" for c in ["m","n"] for a in ["m","n"] for target in targets]]
+  labels = ["(C-)xxx(-target)", "(C-)nxx(-target)", "(C-)xnx(-target)", "(C-)xxn(-target)"]
+  colors = ["tab:green", "wheat", "skyblue", "salmon"]
+  linestyles = ["solid", "dashed", "dashed", "dashed"]
+  system.plot(compartments = ["plasma"] + [f"{tumor['name']}_interstitial" for tumor in system.tumors] + [f"{organ['name']}_interstitial" for organ in system.organs], 
+              groups = groups, labels = labels, colors = colors, linestyles = linestyles,
+              output = f"{name}_drugs.png")
+  
+  groups = [["A"], ["B"], drugs,
+            [f"{drug}-A" for drug in drugs] + [f"C-{drug}-A" for drug in drugs],
+            [f"{drug}-B" for drug in drugs] + [f"C-{drug}-B" for drug in drugs],
+            [f"{drug}-AB" for drug in drugs] + [f"C-{drug}-AB" for drug in drugs]]
+  labels = ["A", "B", "(C-)drug", "(C-)drug-A", "(C-)drug-B", "(C-)drug-AB"]
+  colors = ["tab:blue", "tab:red", "black", "skyblue", "salmon", "tab:purple"]
+  system.plot(compartments = ["plasma"] + [f"{tumor['name']}_interstitial" for tumor in system.tumors] + [f"{organ['name']}_interstitial" for organ in system.organs],
+              groups = groups, labels = labels, colors = colors,
+              output = f"{name}_targets.png")
 
-    groups = [["A"], ["B"], drugs,
-              [f"{drug}-A" for drug in drugs] + [f"C-{drug}-A" for drug in drugs],
-              [f"{drug}-B" for drug in drugs] + [f"C-{drug}-B" for drug in drugs],
-              [f"{drug}-AB" for drug in drugs] + [f"C-{drug}-AB" for drug in drugs]]
-    labels = ["A", "B", "(C-)drug", "(C-)drug-A", "(C-)drug-B", "(C-)drug-AB"]
-    colors = ["tab:blue", "tab:red", "black", "skyblue", "salmon", "tab:purple"]
-    system.plot(compartments = ["tumor_interstitial", "other_interstitial", "lung_interstitial", "SI_interstitial"],
-                groups = groups, labels = labels, colors = colors,
-                output = f"{dose}_{construct_name}_targets.png")
+
+system = model(human, VIB4, [double, single], [other, lung, SI], connect_tumors = True)
+system.add_x("mmm", "plasma", 1 * units.nM)
+system.run(300 * units.h, t_step = 1/60 * units.h, t_record = 1 * units.h)
+plot(system, "VIB4_1nM")
