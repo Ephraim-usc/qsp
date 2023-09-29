de4461870618143f38da834e9dec5e833694ca4a
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-09-29 21:56:59 +0800

    Update run.py

--- qsp/run.py
+++ qsp/run.py
@@ -25,12 +25,12 @@ def plot(system, name):
               output = f"{name}_summary.png")
   
   groups = [drugs + [f"{drug}-{target}" for drug in drugs for target in targets] + [f"C-{drug}" for drug in drugs] + [f"C-{drug}-{target}" for drug in drugs for target in targets],
-                [f"n{a}{b}" for a in ["m","n"] for b in ["m","n"]] + [f"n{a}{b}-{target}" for a in ["m","n"] for b in ["m","n"] for target in targets] + [f"C-n{a}{b}" for a in ["m","n"] for b in ["m","n"]] + [f"C-n{a}{b}-{target}" for a in ["m","n"] for b in ["m","n"] for target in targets],
-                [f"{c}n{b}" for c in ["m","n"] for b in ["m","n"]] + [f"{c}n{b}-{target}" for c in ["m","n"] for b in ["m","n"] for target in targets] + [f"C-{c}n{b}" for c in ["m","n"] for b in ["m","n"]] + [f"C-{c}n{b}-{target}" for c in ["m","n"] for b in ["m","n"] for target in targets],
-                [f"{c}{a}n" for c in ["m","n"] for a in ["m","n"]] + [f"{c}{a}n-{target}" for c in ["m","n"] for a in ["m","n"] for target in targets] + [f"C-{c}{a}n" for c in ["m","n"] for a in ["m","n"]] + [f"C-{c}{a}n-{target}" for c in ["m","n"] for a in ["m","n"] for target in targets]]
-  labels = ["(C-)xxx(-target)", "(C-)nxx(-target)", "(C-)xnx(-target)", "(C-)xxn(-target)"]
-  colors = ["tab:green", "wheat", "skyblue", "salmon"]
-  linestyles = ["solid", "dashed", "dashed", "dashed"]
+            [f"n{a}{b}" for a in ["m","n"] for b in ["m","n"]] + [f"n{a}{b}-{target}" for a in ["m","n"] for b in ["m","n"] for target in targets] + [f"C-n{a}{b}" for a in ["m","n"] for b in ["m","n"]] + [f"C-n{a}{b}-{target}" for a in ["m","n"] for b in ["m","n"] for target in targets],
+            [f"{c}n{b}" for c in ["m","n"] for b in ["m","n"]] + [f"{c}n{b}-{target}" for c in ["m","n"] for b in ["m","n"] for target in targets] + [f"C-{c}n{b}" for c in ["m","n"] for b in ["m","n"]] + [f"C-{c}n{b}-{target}" for c in ["m","n"] for b in ["m","n"] for target in targets],
+            ["a"]]
+  labels = ["(C-)xxx(-target)", "(C-)nx(-target)", "(C-)xn(-target)", "cap"]
+  colors = ["tab:green", "wheat", "skyblue", "salmon", "black"]
+  linestyles = ["solid", "dashed", "dashed", "dashed", "dotted"]
   system.plot(compartments = ["plasma"] + [f"{tumor['name']}_interstitial" for tumor in system.tumors] + [f"{organ['name']}_interstitial" for organ in system.organs], 
               groups = groups, labels = labels, colors = colors, linestyles = linestyles,
               output = f"{name}_drugs.png")
@@ -50,3 +50,5 @@ system = model(human, VIB4, [double, single], [other, lung, SI], connect_tumors 
 system.add_x("mm", "plasma", 1 * units.nM)
 system.run(300 * units.h, t_step = 1/60 * units.h, t_record = 1 * units.h)
 plot(system, "VIB4_1nM")
+
+
