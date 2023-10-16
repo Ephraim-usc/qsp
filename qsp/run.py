a0e02ed254dedad2df1b6f3a8b314d7c42fe0c12
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-10-16 11:55:34 +0800

    Update run.py

--- qsp/run.py
+++ qsp/run.py
@@ -17,24 +17,6 @@ def plot(system, name):
   #pickle.dump(system, open(f"{name}.pickle", "wb"))
   
   targets = ["A", "B", "AB"]
-  groups = [["C"], ["A"], ["B"], ["a"],
-            drugs,
-            [f"C-{drug}" for drug in drugs],
-            [f"{drug}-{target}" for drug in drugs for target in targets],
-            [f"C-{drug}-{target}" for drug in drugs for target in targets]]
-  labels = ["C", "target", "cap", "drug", "C-drug", "drug-target", "C-drug-target"]
-  colors = ["tab:orange", "tab:blue", "black", "black", "wheat", "skyblue", "tab:purple"]
-  linestyles = ["solid", "solid", "dotted", "solid", "solid", "solid", "solid"]
-  system.plot(compartments = ["plasma"] + [f"{tumor['name']}_interstitial" for tumor in system.tumors] + [f"{organ['name']}_interstitial" for organ in system.organs], 
-              groups = groups, labels = labels, colors = colors, linestyles = linestyles,
-              output = f"{name}_summary.png")
-
-
-
-def plot(system, name):
-  #pickle.dump(system, open(f"{name}.pickle", "wb"))
-  
-  targets = ["A", "B", "AB"]
   groups = [["C"],
             ["A", "B"],
             ["a"],
@@ -52,23 +34,38 @@ def plot(system, name):
   groups = [drugs + [f"{drug}-{target}" for drug in drugs for target in targets] + [f"C-{drug}" for drug in drugs] + [f"C-{drug}-{target}" for drug in drugs for target in targets],
             [f"n{a}" for a in ["m","n"]] + [f"n{a}-{target}" for a in ["m","n"] for target in targets] + [f"C-n{a}" for a in ["m","n"]] + [f"C-n{a}-{target}" for a in ["m","n"] for target in targets],
             [f"{c}n" for c in ["m","n"]] + [f"{c}n-{target}" for c in ["m","n"] for target in targets] + [f"C-{c}n" for c in ["m","n"]] + [f"C-{c}n-{target}" for c in ["m","n"] for target in targets],
-            ["a"]]
-  labels = ["(C-)xxx(-target)", "(C-)nx(-target)", "(C-)xn(-target)", "cap"]
-  colors = ["tab:green", "wheat", "skyblue", "black"]
-  linestyles = ["solid", "dashed", "dashed", "dotted"]
-  system.plot(compartments = ["plasma"] + [f"{tumor['name']}_interstitial" for tumor in system.tumors] + [f"{organ['name']}_interstitial" for organ in system.organs], 
+            [f"{c}a" for c in ["m","n"]] + [f"{c}a-{target}" for c in ["m","n"] for target in targets] + [f"C-{c}a" for c in ["m","n"]] + [f"C-{c}a-{target}" for c in ["m","n"] for target in targets]]
+  labels = ["(C-)xxx(-target)", "(C-)nx(-target)", "(C-)xn(-target)", "(C-)xa(-target)"]
+  colors = ["tab:green", "wheat", "skyblue", "salmon"]
+  linestyles = ["solid", "dashed", "dashed", "dashed"]
+  system.plot(compartments = ["plasma"] + [tumor["name"] for tumor in system.tumors] + [organ["name"] for organ in system.organs], 
               groups = groups, labels = labels, colors = colors, linestyles = linestyles,
               output = f"{name}_drugs.png")
-  
-  groups = [["A"], ["B"], drugs,
-            [f"{drug}-A" for drug in drugs] + [f"C-{drug}-A" for drug in drugs],
-            [f"{drug}-B" for drug in drugs] + [f"C-{drug}-B" for drug in drugs],
-            [f"{drug}-AB" for drug in drugs] + [f"C-{drug}-AB" for drug in drugs]]
-  labels = ["A", "B", "(C-)drug", "(C-)drug-A", "(C-)drug-B", "(C-)drug-AB"]
-  colors = ["tab:blue", "tab:red", "black", "skyblue", "salmon", "tab:purple"]
-  system.plot(compartments = ["plasma"] + [f"{tumor['name']}_interstitial" for tumor in system.tumors] + [f"{organ['name']}_interstitial" for organ in system.organs],
-              groups = groups, labels = labels, colors = colors,
-              output = f"{name}_targets.png")
+
+
+for interrnalization_tumor in [0.0, 0.01, 0.02, 0.05, 0.1, 0.15, 0.2]:
+  TCE = VIB4.copy()
+  TCE["interrnalization_tumor"] = interrnalization_tumor / units.h
+  system = model(human, TCE, [tumor_AB, tumor_A, tumor_B], [other, lung, SI], connect_tumors = True)
+  system.add_x("mm", "plasma", 10 * units.nM)
+  system.run(300 * units.h, t_step = 1/60 * units.h, t_record = 1 * units.h)
+  plot(system, f"{interrnalization_tumor*100}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 params = [[0.05, 0.15, 0.05, 0.15, x] for x in [1e-12, 1e-11, 1e-10, 1e-9, 1e-8, 1e-7]] + \
