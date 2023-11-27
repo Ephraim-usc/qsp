83d95bddd25f72033d08ae6e92b2abc5cec721c8
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-11-27 16:42:30 +0800

    Update model_TCE_20231127.py

--- qsp/model_TCE_20231127.py
+++ qsp/model_TCE_20231127.py
@@ -4,7 +4,7 @@ import re
 # this is adapted from the standard TCE model except that
 # the 153 cap has affinity to the 153 head
 # the 153 mask has low masking effect on the 305 head
-
+# tumor infiltrating T cells are fewer
 
 
 effectors = ["C"]; targets = ["A", "B", "AB"]
@@ -183,7 +183,7 @@ UT44.update({"volume": 170 * units.microliter, "volume_plasma_proportion": 0.07,
 UT44.update({"plasma_flow_density": 12.7 / units.h, "lymphatic_flow_ratio": 0.002})
 UT44.update({"capillary_radius": 10 * units.micrometer, "capillary_permeability": 3e-7 * units.cm/units.s})
 UT44.update({"diffusion": 10 * units.micrometer**2 / units.s})
-UT44.update({"density_cell": 3e8 * 0.44 / units.ml, "density_T": 3e8 * 0.15 / units.ml, "density_NK": 3e8 * 0.02 / units.ml})
+UT44.update({"density_cell": 3e8 * 0.44 / units.ml, "density_T": 3e8 * 0.015 / units.ml, "density_NK": 3e8 * 0.02 / units.ml})
 UT44.update({"num_A": 7e5, "num_B": 1.45e6})
 
 FTC238 = {"name": "tumor"}
@@ -191,7 +191,7 @@ FTC238.update({"volume": 170 * units.microliter, "volume_plasma_proportion": 0.0
 FTC238.update({"plasma_flow_density": 12.7 / units.h, "lymphatic_flow_ratio": 0.002})
 FTC238.update({"capillary_radius": 10 * units.micrometer, "capillary_permeability": 3e-7 * units.cm/units.s})
 FTC238.update({"diffusion": 10 * units.micrometer**2 / units.s})
-FTC238.update({"density_cell": 3e8 * 0.44 / units.ml, "density_T": 3e8 * 0.15 / units.ml, "density_NK": 3e8 * 0.02 / units.ml})
+FTC238.update({"density_cell": 3e8 * 0.44 / units.ml, "density_T": 3e8 * 0.015 / units.ml, "density_NK": 3e8 * 0.02 / units.ml})
 FTC238.update({"num_A": 1e5, "num_B": 1e5})
 
 
