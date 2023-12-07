9b6c63a68115465ecaeb0b9a2b998dc79042e960
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2023-12-07 14:21:35 +0800

    Update qsp.py

--- qsp/qsp.py
+++ qsp/qsp.py
@@ -367,7 +367,9 @@ class System:
     pbar.close()
     print(f"time in computing flows: {A:.8f}s\ntime in computing reactions: {B:.8f}s\ntime in computing reactions: {C:.8f}s\ntime in computing processes: {D:.8f}s\n", flush = True)
   
-  def plot(self, compartments, groups = None, labels = None, colors = None, linestyles = None, output = None):
+  def plot(self, compartments = None, groups = None, labels = None, colors = None, linestyles = None, linthresh = 1e-3, output = None):
+    if compartment is None:
+      compartment = self.compartments
     compartments = [self.compartments.index(compartment) for compartment in compartments]
     
     if groups is None:
@@ -401,8 +403,8 @@ class System:
       else:
         ax.set_xticks([10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
       ax.set_xlim(0, Xmax)
-      ax.set_yscale('symlog', linthresh = 1e-3)
-      ax.set_yticks([1e-3, 1e-2, 1e-1, 0, 1, 10, 100, 1000, 10000, 1e5, 1e6])
+      ax.set_yscale('symlog', linthresh = linthresh)
+      ax.set_yticks([y for y in [1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 0, 1, 10, 100, 1000, 10000, 1e5, 1e6] if y >= linthresh])
       ax.set_ylim(0, Ymax)
       ax.grid(axis = "y", color = "grey", linewidth = 1)
       ax.set_title(self.compartments[compartment])
