cef82d80b79bee5a0143685c33f39dabf8552e3f
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-01-11 16:54:38 +0800

    Update model_TAA_IL2.py

--- qsp/model_TAA_IL2.py
+++ qsp/model_TAA_IL2.py
@@ -152,7 +152,7 @@ UT44.update({"plasma_flow_density": 12.7 / units.h, "lymphatic_flow_ratio": 0.00
 UT44.update({"capillary_radius": 10 * units.micrometer, "capillary_permeability": 3e-7 * units.cm/units.s})
 UT44.update({"diffusion": 10 * units.micrometer**2 / units.s})
 UT44.update({"density_cell": 3e8 * 0.44 / units.ml, "density_T": 3e8 * 0.15 / units.ml, "density_NK": 3e8 * 0.02 / units.ml})
-UT44.update({"num_A": 7e5, "num_B": 1.45e6})
+UT44.update({"num_A": 1e5, "num_B": 0})
 
 FTC238 = {"name": "tumor"}
 FTC238.update({"volume": 170 * units.microliter, "volume_plasma_proportion": 0.07, "volume_interstitial_proportion": 0.55})
@@ -160,7 +160,7 @@ FTC238.update({"plasma_flow_density": 12.7 / units.h, "lymphatic_flow_ratio": 0.
 FTC238.update({"capillary_radius": 10 * units.micrometer, "capillary_permeability": 3e-7 * units.cm/units.s})
 FTC238.update({"diffusion": 10 * units.micrometer**2 / units.s})
 FTC238.update({"density_cell": 3e8 * 0.44 / units.ml, "density_T": 3e8 * 0.15 / units.ml, "density_NK": 3e8 * 0.02 / units.ml})
-FTC238.update({"num_A": 1e5, "num_B": 1e5})
+FTC238.update({"num_A": 1e5, "num_B": 0})
 
 
 ############ organs ############
