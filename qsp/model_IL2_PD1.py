58bf30124facb175d9e403c65cb2a77ea6a8a914
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-02-07 19:23:07 +0800

    Update model_IL2_PD1.py

--- qsp/model_IL2_PD1.py
+++ qsp/model_IL2_PD1.py
@@ -403,12 +403,12 @@ TREG_RATIO = 0.1
 Treg = Cell("Treg", ["P", "α"], [30000, 300], [0.05/units.h, 2.0/units.h],
             birth = SIGNALS_ENV["tumor"] * 0.01 / units.d * tumor_cell_total_density * 0.1 * TREG_RATIO,
             death = SIGNALS_ENV["tumor"] * 0.01 / units.d,
-            prolif = SIGNALS_ENV["tumor"] * 0.5 / units.d * (0.05 - hill(30000 - SIGNALS_CEL["P"], 10000, EMAX = 0.1) + hill(SIGNALS_CEL["α"], 100, coef = 1, EMAX = 0.3)))
+            prolif = SIGNALS_ENV["tumor"] * 0.5 / units.d * (0.05 - hill(30000 - SIGNALS_CEL["P"], 10000, EMAX = 0.1) + hill(SIGNALS_CEL["α"], 100, coef = 1, EMAX = 0.2)))
 Th = Cell("Th", ["P", "R"], [30000, 300], [0.05/units.h, 2.0/units.h],
           birth = SIGNALS_ENV["tumor"] * 0.01 / units.d * tumor_cell_total_density * 0.1 * (1-TREG_RATIO),
           death = SIGNALS_ENV["tumor"] * 0.01 / units.d,
           prolif = SIGNALS_ENV["tumor"] * 0.5 / units.d * (0.05 - hill(30000 - SIGNALS_CEL["P"], 10000, EMAX = 0.1) + hill(SIGNALS_CEL["R"], 100, coef = 1, EMAX = 0.2)),
-          diff = SIGNALS_ENV["tumor"] * 0.1 / units.d * hill(SIGNALS_CEL["R"], EC50 = 100, coef = 1.0, EMAX = 0.2),
+          diff = SIGNALS_ENV["tumor"] * 0.2 / units.d * hill(SIGNALS_CEL["R"], EC50 = 100, coef = 1.0, EMAX = 0.3),
           diff_cell = Treg)
 Tm = Cell("Tm", ["P", "R"], [30000, 1500], [0.05/units.h, 2.0/units.h])
 Tex = Cell("Tex", ["P", "α"], [60000, 1500], [0.05/units.h, 2.0/units.h],
