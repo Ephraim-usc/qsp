257c80d9ad2cba4e4147b244df9fb3bbb439dc63
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-02-22 19:23:38 +0800

    Update plots.py

--- qsp/plots.py
+++ qsp/plots.py
@@ -1,6 +1,6 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import matplotlib.colors as clr
+import matplotlib.colors as mcolors
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 import numpy as np
