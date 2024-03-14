43aa985894e96d8787480d7cb01c9cb43be07fb6
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-03-14 14:59:19 +0800

    Update model_TCE_PD1.py

--- qsp/model_TCE_PD1.py
+++ qsp/model_TCE_PD1.py
@@ -75,32 +75,21 @@ class transform:
 
 
 class internalization:
-  def __init__(self, rates_effector, rates_target, compartments = None):
+  def __init__(self, rates, compartments = None):
     self.system = None
     self.compartments = compartments
     
-    q_effector = np.zeros(len(dimers_effector))
-    Q_effector = np.zeros([len(dimers_effector), len(antigens_effector)])
-    for effector, antigens, rate in rates_effector:
-      idx_effector = [dimers_effector.index(f"{effector}-{drug}") for drug in drugs]
-      idx_antigens = [antigens_effector.index(antigen) for antigen in antigens]
-      q_effector[idx_effector] -= rate.number(1/units.h)
-      for i in idx_effector:
-        np.add.at(Q_effector, (i, idx_antigens), 1)
-    
-    q_target = np.zeros(len(dimers_target))
-    Q_target = np.zeros([len(dimers_target), len(antigens_target)])
-    for target, antigens, rate in rates_target:
-      idx_target = [dimers_target.index(f"{drug}-{target}") for drug in drugs]
-      idx_antigens = [antigens_target.index(antigen) for antigen in antigens]
-      q_target[idx_target] -= rate.number(1/units.h)
+    q = np.zeros(len(dimers))
+    Q = np.zeros([len(dimers), len(antigens)])
+    for target, antigens, rate in rates:
+      idx_target = [dimers.index(f"{drug}-{target}") for drug in drugs]
+      idx_antigens = [antigens.index(antigen) for antigen in antigens]
+      q[idx_target] -= rate.number(1/units.h)
       for i in idx_target:
-        np.add.at(Q_target, (i, idx_antigens), 1) # there may be repeated antigens
+        np.add.at(Q, (i, idx_antigens), 1) # there may be repeated antigens
     
-    self.q_effector = q_effector
-    self.Q_effector = Q_effector
-    self.q_target = q_target
-    self.Q_target = Q_target
+    self.q = q
+    self.Q = Q
   
   def __call__(self, system, t):
     if self.system is not system:
@@ -119,54 +108,12 @@ class internalization:
       self.idx_antigens_target = [system.analytes.index(analyte) for analyte in antigens_target]
     
     for compartment in self.compartments_:
-      delta_effector = system.x[self.idx_dimers_effector, compartment] * (1 - np.exp(self.q_effector * t.number(units.h)))
-      system.x[self.idx_dimers_effector, compartment] -= delta_effector
-      system.x[self.idx_antigens_effector, compartment] += delta_effector @ self.Q_effector
-      
       delta_target = system.x[self.idx_dimers_target, compartment] * (1 - np.exp(self.q_target * t.number(units.h)))
       system.x[self.idx_dimers_target, compartment] -= delta_target
       system.x[self.idx_antigens_target, compartment] += delta_target @ self.Q_target
 
 
 ############ drugs ############
-
-VIB6 = {}
-VIB6.update({"off_C": 10**-4 / units.s, "affn_C": 10 * units.nM, "affm_C": 200 * units.nM})
-VIB6.update({"off_A": 10**-4 / units.s, "affn_A": 10 * units.nM, "affm_A": 200 * units.nM})
-VIB6.update({"off_B": 10**-4 / units.s, "affn_B": 10 * units.nM, "affm_B": 200 * units.nM})
-VIB6.update({"avidity_effector": 19, "avidity_target": 19})
-VIB6.update({"clearance": math.log(2)/(70 * units.h)}); VIB6["smalls"] = []
-VIB6["cleavage"] = transform(reactants = ["m.."] + [".m."] + ["..m"], products = ["n.."] + [".n."] + ["..n"],
-                             rates = [("tumor_AB", 0.15 / units.d), 
-                                      ("tumor_A", 0.15 / units.d), 
-                                      ("tumor_B", 0.15 / units.d), 
-                                      ("plasma", 0.01 / units.d), 
-                                      ("liver", 0.03 / units.d), 
-                                      ("lung", 0.01 / units.d), 
-                                      ("SI", 0.05 / units.d), 
-                                      ("gallbladder", 0.03 / units.d)])
-VIB6["internalization"] = internalization(rates_effector = [("C", ["C"], 0.1 / units.h)],
-                                          rates_target = [("A", ["A"], 0.02 / units.h), ("B", ["B"], 0.02 / units.h), ("AB", ["A", "B"], 0.02 / units.h)])
-
-
-JANX008 = {}
-JANX008.update({"off_C": 10**-4 / units.s, "affn_C": 0.2 * units.nM, "affm_C": 100 * units.nM})
-JANX008.update({"off_A": 10**-4 / units.s, "affn_A": 0.5 * units.nM, "affm_A": 30 * units.nM})
-JANX008.update({"off_B": 10**-4 / units.s, "affn_B": math.inf * units.nM, "affm_B": math.inf * units.nM})
-JANX008.update({"avidity_effector": 19, "avidity_target": 19})
-JANX008.update({"clearance": math.log(2)/(50 * units.h)}); JANX008["smalls"] = ["nmm", "nmn", "nnm", "nnn"]
-JANX008["cleavage"] = transform(reactants = ["m.."] + [".m."] + ["..m"], products = ["n.."] + [".n."] + ["..n"],
-                             rates = [("tumor_AB", 0.15 / units.d), 
-                                      ("tumor_A", 0.15 / units.d), 
-                                      ("tumor_B", 0.15 / units.d), 
-                                      ("plasma", 0.07 / units.d), 
-                                      ("liver", 0.07 / units.d), 
-                                      ("lung", 0.07 / units.d), 
-                                      ("SI", 0.07 / units.d), 
-                                      ("gallbladder", 0.07 / units.d)])
-JANX008["internalization"] = internalization(rates_effector = [("C", ["C"], 0.1 / units.h)],
-                                             rates_target = [("A", ["A"], 0.02 / units.h), ("B", ["B"], 0.02 / units.h), ("AB", ["A", "B"], 0.02 / units.h)])
-
 
 VIB7P = {}
 VIB7P.update({"off_P": 10**-4 / units.s, "aff_P": 1 * units.nM})
@@ -176,8 +123,12 @@ VIB7P.update({"off_B": 10**-4 / units.s, "affn_B": math.inf * units.nM, "affm_B"
 VIB7P.update({"avidity_effector": 19, "avidity_target": 19})
 VIB7P.update({"clearance": math.log(2)/(50 * units.h)}); VIB7P["smalls"] = []
 
-VIB7P["internalization"] = internalization(rates_effector = [("C", ["C"], 0.1 / units.h)],
-                                             rates_target = [("A", ["A"], 0.02 / units.h), ("B", ["B"], 0.02 / units.h), ("AB", ["A", "B"], 0.02 / units.h)])
+VIB7P["internalization"] = internalization(rates = [("P", ["P"], 0.02 / units.h),
+                                                    ("C", ["C"], 0.1 / units.h),
+                                                    ("PC", ["P", "C"], 0.01 / units.h),
+                                                    ("A", ["A"], 0.02 / units.h),
+                                                    ("B", ["B"], 0.02 / units.h),
+                                                    ("AB", ["A", "B"], 0.01 / units.h)])
 
 linker = [("tumor_AB", 0.15 / units.d), 
           ("tumor_A", 0.15 / units.d), 
