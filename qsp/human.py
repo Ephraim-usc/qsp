05a093ddf8346b6e8c88d9fd0957626ec278186d
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-04-01 14:14:35 +0800

    Update human.py

--- qsp/human.py
+++ qsp/human.py
@@ -4,11 +4,13 @@ plasma = {"name": "plasma"}
 plasma.update({"volume": 3126 * units.ml})
 plasma.update({"num_T": 7.9E+09, "num_NK": 1.6E+09})
 plasma.update({"conc_A": 5 * units.ug/units.l / (170 * units.kDa), "conc_B": 0 * units.nM})
+plasma.update({"conc_HER2": 980 * units.ng/units.l / (185 * units.kDa)})
 
 lymph = {"name": "lymph"}
 lymph.update({"volume": 274 * units.ml})
 lymph.update({"num_T": 3.6E+11, "num_NK": 6.7E+08})
 lymph.update({"conc_A": 0 * units.nM, "conc_B": 0 * units.nM})
+lymph.update({"conc_HER2": 0 * units.nM})
 
 bone = {"name": "bone"}
 bone.update({"volume_plasma": 224 * units.ml, "volume_interstitial": 1891 * units.ml})
@@ -16,6 +18,7 @@ bone.update({"plasma_flow": 2591 * units.ml/units.h, "lymphatic_flow_ratio": 0.0
 bone.update({"vascular_reflection": 0.85, "lymphatic_reflection": 0.2})
 bone.update({"num_cell": 4.77E+09 * 0.5, "num_T": 2.1E+10, "num_NK": 3.3E+09})
 bone.update({"num_A": 0, "num_B": 0})
+bone.update({"num_HER2": 0})
 
 lung = {"name": "lung"}
 lung.update({"volume_plasma": 55 * units.ml, "volume_interstitial": 300 * units.ml})
@@ -23,6 +26,7 @@ lung.update({"vascular_reflection": 0.95, "lymphatic_reflection": 0.2})
 lung.update({"plasma_flow": 181913 * units.ml/units.h, "lymphatic_flow_ratio": 0.002})
 lung.update({"num_cell": 2.36E+11 * 0.5, "num_T": 1.3E+10, "num_NK": 7.2E+08})
 lung.update({"num_A": 1000, "num_B": 0}) # num_B = 1019 from Liyuan
+lung.update({"num_HER2": 12684})
 
 liver = {"name": "liver"}
 liver.update({"volume_plasma": 183 * units.ml, "volume_interstitial": 429 * units.ml})
@@ -30,6 +34,7 @@ liver.update({"vascular_reflection": 0.85, "lymphatic_reflection": 0.2})
 liver.update({"plasma_flow": 13210 * units.ml/units.h, "lymphatic_flow_ratio": 0.002})
 liver.update({"num_cell": 3.90E+11 * 0.5, "num_T": 7.9E+09, "num_NK": 5.5E+09})
 liver.update({"num_A": 10000, "num_B": 0})
+liver.update({"num_HER2": 12005})
 
 SI = {"name": "SI"}
 SI.update({"volume_plasma": 6.15 * units.ml, "volume_interstitial": 67.1 * units.ml})
@@ -37,6 +42,7 @@ SI.update({"vascular_reflection": 0.9, "lymphatic_reflection": 0.2})
 SI.update({"plasma_flow": 12368 * units.ml/units.h, "lymphatic_flow_ratio": 0.002})
 SI.update({"num_cell": 7.2e11 * 0.5, "num_T": 1.8E+10, "num_NK": 8.1E+08})
 SI.update({"num_A": 1000, "num_B": 10000})
+SI.update({"num_HER2": 58378})
 
 skin = {"name": "skin"}
 skin.update({"volume_plasma": 127 * units.ml, "volume_interstitial": 1125 * units.ml})
@@ -44,6 +50,7 @@ skin.update({"vascular_reflection": 0.95, "lymphatic_reflection": 0.2})
 skin.update({"plasma_flow": 11626 * units.ml/units.h, "lymphatic_flow_ratio": 0.002})
 skin.update({"num_cell": 6.12E+11 * 0.5, "num_T": 2.7E+10, "num_NK": 1.9E+09})
 skin.update({"num_A": 10000, "num_B": 0})
+skin.update({"num_HER2": 15621})
 
 kidney = {"name": "kidney"}
 kidney.update({"volume_plasma": 18.2 * units.ml, "volume_interstitial": 49.8 * units.ml})
@@ -51,6 +58,7 @@ kidney.update({"vascular_reflection": 0.9, "lymphatic_reflection": 0.2})
 kidney.update({"plasma_flow": 36402 * units.ml/units.h, "lymphatic_flow_ratio": 0.002})
 kidney.update({"num_cell": 1.06E+11 * 0.5, "num_T": 496504047, "num_NK": 1023295100}) # T anc NK numbers are calculated as density * volume
 kidney.update({"num_A": 10000, "num_B": 0})
+kidney.update({"num_HER2": 35005})
 
 gallbladder = {"name": "gallbladder"}
 gallbladder.update({"volume_plasma": 18.2/4 * units.ml, "volume_interstitial": 49.8/4 * units.ml})
@@ -58,6 +66,15 @@ gallbladder.update({"vascular_reflection": 0.9, "lymphatic_reflection": 0.2})
 gallbladder.update({"plasma_flow": 36402/4 * units.ml/units.h, "lymphatic_flow_ratio": 0.002})
 gallbladder.update({"num_cell": 1.06E+11 * 0.5/4, "num_T": 496504047/4, "num_NK": 1023295100/4}) # T anc NK numbers are calculated as density * volume
 gallbladder.update({"num_A": 10000, "num_B": 100000})
+gallbladder.update({"num_HER2": 33165})
+
+heart = {"name": "heart"}
+heart.update({"volume_plasma": 13.1 * units.ml, "volume_interstitial": 48.8 * units.ml})
+heart.update({"vascular_reflection": 0.95, "lymphatic_reflection": 0.2})
+heart.update({"plasma_flow": 7752 * units.ml/units.h, "lymphatic_flow_ratio": 0.002})
+heart.update({"num_cell": 5.64E+10 * 0.5, "num_T": 18211368, "num_NK": 7249125}) # T anc NK numbers are calculated as density * volume
+heart.update({"num_A": 4441, "num_B": 6609})
+heart.update({"num_HER2": 33165})
 
 other = {"name": "other"}
 other.update({"volume_plasma": 1000 * units.ml, "volume_interstitial": 5000 * units.ml})
@@ -65,3 +82,4 @@ other.update({"plasma_flow": 100000 * units.ml/units.h, "lymphatic_flow_ratio": 
 other.update({"vascular_reflection": 0.95, "lymphatic_reflection": 0.2})
 other.update({"num_cell": 1e13 * 0.5, "num_T": 1.1E+10, "num_NK": 3.1E+09})
 other.update({"num_A": 10000, "num_B": 0})
+other.update({"num_HER2": 10000})
