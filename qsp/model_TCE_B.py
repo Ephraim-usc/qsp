8cbfa90391d6d057c585b24d8b9549c800223178
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-04-28 22:49:59 +0800

    Update model_TCE_B.py

--- qsp/model_TCE_B.py
+++ qsp/model_TCE_B.py
@@ -106,14 +106,15 @@ class internalization:
 
 
 contact_area_time = 10*units.s * math.pi*units.um**2
-contact_freq = 4 * math.pi * (0.1075 * units.um**2 / units.s) * 4*units.um # T cell diffusion rate according to https://pubmed.ncbi.nlm.nih.gov/29044117/ Figure 2D
+contact_freq = 4 * math.pi * (6.45 * units.um**2 / units.MIN) * 4*units.um # T cell diffusion rate according to https://pubmed.ncbi.nlm.nih.gov/29044117/ Figure 2D
 
 class kill:
-  def __init__(self, compartments, on2ds, contact_area_time = contact_area_time, contact_freq = contact_freq, synapse_efficiency = 0.01, damage = 0.5, regen = 0.1 / units.h):
+  def __init__(self, compartments, on2ds, 
+               effector = "T", target = "B", contact_area_time = contact_area_time, contact_freq = contact_freq, synapse_efficiency = 0.01, damage = 0.5, regen = 0.1 / units.h):
     self.system = None
     self.compartments = compartments
     self.on2ds = on2ds # pandas data frame of unit um**2/s
-    self.contact_freq = contact_freq.number(units.um**3 / units.s)
+    self.contact_freq = contact_freq.number(units.ml / units.h)
     self.contact_area_time = contact_area_time.number(units.um**2 * units.s)
     self.regen = regen
   
@@ -122,16 +123,21 @@ class kill:
       self.system = system
       self.compartments_ = [system.compartments.index(compartment) for compartment in self.compartments]
       self.hp = np.ones(100000)
-      
+
       ligands_effector = [ligand for ligand in self.on2ds.index.values if ligand in system.analytes]
       ligands_target = [ligand for ligand in self.on2ds.columns.values if ligand in system.analytes]
       
+      self.effector_ = system.cells.index(effector)
       self.ligands_effector_ = [system.analytes.index(ligand) for ligand in ligands_effector]
       self.ligands_target_ = [system.analytes.index(ligand) for ligand in ligands_target]
       self.on2ds_ = self.on2ds.loc[ligands_effector, ligands_target]
     
     trimers = self.contact_area_time * np.array([system.y[self.ligands_effector_, compartment_] @ self.on2ds_ @ system.y[self.ligands_target_, compartment_] for compartment_ in self.compartments_])
-    probs = 0
+    probs = 1 - (1 - synapse_efficiency)**trimers
+    
+    contacts = self.contact_freq * system.c[self.effector_, self.compartments_]
+
+    
     
     
 
