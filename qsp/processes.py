42ddd0010ce94d754ac0d713ac6e30df3f986944
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-05-16 17:06:57 +0800

    Update processes.py

--- qsp/processes.py
+++ qsp/processes.py
@@ -37,35 +37,9 @@ class Equilibrium:
 class Transform:
   def __init__(self):
     self.system = None
-    self.Qs = dict()
-    
-    self.analyteses_ = []
-    self.analyteses_.append([analytes.index(f"{drug}") for drug in drugs])
-    for binding in bindings:
-      self.analyteses_.append([analytes.index(f"{binding}-{drug}") for drug in drugs])
+    self.data = dict()
   
-  def add(self, linker, reactant, products):
-    self.system = None
-    reactant_ = drugs.index(reactant)
-    products_ = [drugs.index(product) for product in products] 
-    
-    for compartment, rate in linker:
-      if compartment not in self.Qs:
-        self.Qs[compartment] = np.zeros([len(drugs), len(drugs)])
-      self.Qs[compartment][reactant_, reactant_] -= rate.number(1/units.h)
-      self.Qs[compartment][reactant_, products_] += rate.number(1/units.h)
-  
-  def __add__(self, transform2): 
-    buffer = transform()
-    buffer.Qs = add_two_dicts(self.Qs, transform2.Qs)
-    return buffer
-  
-  def __call__(self, system, t):
-    if self.system is not system:
-      self.system = system
-      self.Qs_ = {system.compartments.index(compartment):Q for compartment, Q in self.Qs.items() if compartment in system.compartments}
-    
-    t = t.number(units.h)
-    for compartment_, Q in self.Qs_.items():
-      for analytes_ in self.analyteses_:
-        system.x[analytes_, compartment_] = system.x[analytes_, compartment_] @ expm(Q * t)
+  def add(self, compartment, reactant, products, rate):
+    pass
+
+
