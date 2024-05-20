cbbd4cc8210713007e7ec6d18eb44038f0513e75
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-05-20 16:52:39 +0800

    Update processes.py

--- qsp/processes.py
+++ qsp/processes.py
@@ -53,8 +53,8 @@ def add_cleavage(system, linker, drug_source, drug_dests, bindings):
       analyte_dests = [f"{binding}-{drug_dest}" for drug_dest in drug_dests]
       system.add_transform(compartment, analyte_source, analyte_dests, rate)
 
-def add_internalization(system, binding_source, drug_sources, analyte_dests, rate):
+def add_internalization(system, binding_source, analyte_dests, rate, drugs):
   for compartment in system.compartments:
-    for drug_source in drug_sources:
-      analyte_source = f"{binding_source}-{drug_source}"
+    for drug in drugs:
+      analyte_source = f"{binding_source}-{drug}"
       system.add_transform(compartment, analyte_source, analyte_dests, rate)
