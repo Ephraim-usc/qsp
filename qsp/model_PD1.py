38c621623a088cd8180f51a7edad383a3ea9fdc6
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-05-20 15:10:19 +0800

    Update model_PD1.py

--- qsp/model_PD1.py
+++ qsp/model_PD1.py
@@ -1,4 +1,5 @@
 from .qsp import *
+from .processes import *
 import itertools
 
 ### this model is mostly from ...
@@ -12,18 +13,22 @@ drugs = ["m", "n"]
 dimers = [f"{binding}-{drug}" for binding in bindings for drug in drugs]
 analytes = antigens + drugs + dimers
 
+
+
+### drugs ###
+
+linker_175 = [("plasma", 0.07 / units.d), 
+              ("lymph", 0.07 / units.d), 
+              ("bone", 0.07 / units.d), 
+              ("liver", 0.07 / units.d), 
+              ("lung", 0.07 / units.d), 
+              ("SI", 0.07 / units.d), 
+              ("gallbladder", 0.07 / units.d)]
+
 X = {}
 X.update({"off_P": 10**-4 / units.s, "affn_P": 260 * units.nM, "affm_P": 26000 * units.nM})
 X.update({"clearance": math.log(2)/(80 * units.h), "smalls": []})
-
-X["internalization"] = internalization(rates = [("[T]C", ["[T]C"], 0.1 / units.h),
-                                                 ("[B]A", ["[B]A"], 0.1 / units.h)])
-
-BD["cleavage"] = transform()
-for a in ("m", "n"):
-  BD["cleavage"].add(linker = linker, reactant = f"m{a}", products = [f"n{a}"])
-for c in ("m", "n"):
-  BD["cleavage"].add(linker = linker, reactant = f"{c}m", products = [f"{c}n"])
+X["cleavages"] = [(linker, ["m"], ["n"])]
 
 
 ############ model ############
@@ -48,15 +53,18 @@ def model(TCE, plasma, lymph, organs):
       system.add_flow(drug, organ["name"], "lymph", organ["plasma_flow"] * organ["lymphatic_flow_ratio"] * (1 - organ["lymphatic_reflection"]))
       system.add_flow(drug, "lymph", "plasma", organ["plasma_flow"] * organ["lymphatic_flow_ratio"] * (1 - organ["lymphatic_reflection"]))
   
-  # 
+  # bulk clearance
   for compartment in compartments:
     for drug in drugs:
       system.add_flow(drug, compartment, None, system.get_volume(compartment) * TCE["clearance"])
   
-  # small forms plasma clearance
+  # small molecule clearance
   for small in TCE["smalls"]:
     system.add_flow(small, "plasma", None, system.get_volume("plasma") * math.log(2)/(45 * units.MIN))
   
+  # mask cleavage
+  for linker, drug_source, drug_dests in TCE["cleavages"]:
+    add_cleavage(system, linker, drug_source, drug_dests, bindings)
 
 
 ############# demo ###############
