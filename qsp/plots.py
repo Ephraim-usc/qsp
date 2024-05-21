a7437da485f0b47d4797579de9a3c093ac330de8
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-05-21 11:57:10 +0800

    Update plots.py

--- qsp/plots.py
+++ qsp/plots.py
@@ -6,7 +6,9 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 import numpy as np
 import pandas as pd
 
-norm = mcolors.FuncNorm((lambda x: x/(x+10), lambda y: 10*y/(1-y)), vmin=0, vmax=400)
+midpoint = 10
+maxpoint = 400
+norm = mcolors.FuncNorm((lambda x: x/(x+midpoint), lambda y: midpoint*y/(1-y)), vmin=0, vmax=maxpoint)
 cmap = mcolors.LinearSegmentedColormap.from_list('custom',
                                              [(0,      'black'),
                                               (0.25,   'tab:blue'),
@@ -14,14 +16,14 @@ cmap = mcolors.LinearSegmentedColormap.from_list('custom',
                                               (0.75,   'gold'),
                                               (1,      'tab:orange')], N=256)
 
-def heatmap(df, xcol, ycol, xlabel, ylabel, zcols, zlabels, norm = norm, cmap = cmap, output = None):
-  fig, axs = plt.subplots(1, len(zcols))
+def heatmap(df, xcol, ycol, xlabel, ylabel, zcols, zlabels, norm = norm, cmap = cmap, round = 2, output = None):
+  fig, axs = plt.subplots(1, len(zcols), squeeze = False); axs = axs[0, :]
   for ax, zcol, zlabel in zip(axs, zcols, zlabels):
     data = df.pivot(index = ycol, columns = xcol, values = zcol)
     im = ax.imshow(data, origin = "lower", norm = norm, cmap = cmap)
     for i in range(data.shape[1]):
       for j in range(data.shape[0]):
-        text = ax.text(i, j, np.round(data.iloc[j, i], 2), ha="center", va="center", color="w", fontsize = 6)
+        text = ax.text(i, j, np.round(data.iloc[j, i], round), ha="center", va="center", color="w", fontsize = 6)
     
     ax.set_xticks(np.arange(data.shape[1]), labels = data.columns, fontsize = 6); ax.set_xlabel(xlabel)
     ax.set_yticks(np.arange(data.shape[0]), labels = data.index, fontsize = 6); ax.set_ylabel(ylabel)
