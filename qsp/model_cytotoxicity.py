644448566f8230d19042769560d5dd6fb0b4a6f0
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-12-04 15:40:42 +0800

    Update model_cytotoxicity.py

--- qsp/model_cytotoxicity.py
+++ qsp/model_cytotoxicity.py
@@ -4,7 +4,7 @@ class Drug:
   def __init__(self, num_binding_sites):
     self.num_binding_sites = num_binding_sites
   
-  def add_affinity(self, ligand, aff, aff2D, off):
+  def add_affinity(self, ligand, aff, off):
     pass
   
   def add_cis(self, ligand1, ligand2, on2D):
@@ -20,7 +20,19 @@ drug.add_affinity(2, "CA9", 20*units.nM, 1e-4/units.s)
 drug.add_cis(1, 2, )
 
 
-nM = mole/l = 1e-15 mole/um**3
-mole/um**2 = 1e15 nM * um
 
-(1/um**2) / (1/um**2) / (1/um**2) / s = um**2/s
+
+class Cell:
+  def __init__(self, radius = None, area = None, markers = None, copies = None):
+    self.radius = radius
+    if area is None:
+      self.area = math.pi * radius**2
+    
+    self.markers = markers
+    self.copies = copies
+
+
+Tcell = Cell(5*units.um**2, ["CD3"], [50000])
+
+
+system = System("container", analytes, cells)
