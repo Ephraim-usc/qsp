16e21e1c727dbb583268fc20fd2defd65284a0a5
Caoqi Fan <58720699+Ephraim-usc@users.noreply.github.com>
2024-12-04 17:17:35 +0800

    Update model_cytotoxicity.py

--- qsp/model_cytotoxicity.py
+++ qsp/model_cytotoxicity.py
@@ -4,14 +4,21 @@ class Drug:
   def __init__(self, num_binding_sites):
     self.num_binding_sites = num_binding_sites
   
+  # affinity定义的是自由药物（即状态为空）时结合各ligand的能力，以及脱落至自由状态的速率
   def add_affinity(self, binding_site, ligand, aff, off):
     pass
   
+  # cis定义的是已经在细胞表面时，结合同一细胞各ligand的能力，以及解离（之后仍在细胞上）的速率。这是avidity、cross-arm binding等现象的广义描述
+  # 二维浓度的单位是 1/um^2，因此二维on rate的单位是 um^2/s
   def add_cis(self, binding_site, ligand, on2D, off, complex = None):
     pass
   
+  # trans定义的是已经在细胞表面时，如果此细胞与另一细胞接触，结合对方细胞各ligand的能力，以及解离的速率。这是形成trimer等过程的广义描述。
   def add_trans(self, binding_site, ligand, on2D, off, complex = None):
     pass
+
+  def finalize()
+
 
 X = Drug(3)
 X.add_affinity(0, "CD3", 1*units.nM, 1e-4/units.s)
